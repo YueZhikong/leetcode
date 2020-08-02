384c7d4c7e7814ba937d10c607c01bcefe76ead2
YueZhikong <1332801868@qq.com>
2020-08-02 12:07:11 +0800

    2020.08.02 Competation200 only finish one

---
+++ src/Competation200/Leetcode5475.java
@@ -0,0 +1,24 @@
+package Competation200;
+//统计好三元组
+public class Leetcode5475 {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int num = 0;
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if (Math.abs(arr[i]-arr[j])>a){
+                    continue;
+                }
+                for (int k=j+1;k<arr.length;k++){
+                    if (Math.abs(arr[j]-arr[k])>b){
+                        continue;
+                    }
+                    if (Math.abs(arr[i]-arr[k])>c){
+                        continue;
+                    }
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+}
