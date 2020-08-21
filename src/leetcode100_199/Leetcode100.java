a0b9c84ffb09498f0d5b879ce4b414a332bb8bf6
sunyanyang <sunyanyang@ebupt.com>
2020-08-21 18:32:17 +0800

    2020.08.21

---
+++ src/leetcode100_199/Leetcode100.java
@@ -0,0 +1,19 @@
+package leetcode100_199;
+
+import forOffer.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leetcode100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue_p = new LinkedList<>();
+        Queue<TreeNode> queue_q = new LinkedList<>();
+        queue_p.add(p);
+        queue_q.add(q);
+        while (queue_p!=null&&queue_q!=null){
+
+        }
+        return false;
+    }
+}
