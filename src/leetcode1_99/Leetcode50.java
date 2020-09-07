1c13c2b34637211d2976b5018a54bacf6b2d62b1
sunyanyang <sunyanyang@ebupt.com>
2020-09-07 17:53:21 +0800

    2020.09.07

---
+++ src/leetcode1_99/Leetcode50.java
@@ -0,0 +1,9 @@
+package leetcode1_99;
+
+public class Leetcode50 {
+    //投机取巧
+    public double myPow(double x, int n) {
+        return Math.pow(x,(double)n);
+    }
+    //TODO 还有一种写法，找出n是哪些2^k组成的，然后计算 2 2^2 2^3 .......最后相乘
+}
